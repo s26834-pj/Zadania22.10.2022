344b7a6b8152660aee78fb84033b39fd0abc2ca0
s26834-pj <115928176+s26834-pj@users.noreply.github.com>
2022-11-01 19:40:24 +0100

    Add files via upload

---
+++ Zadanie2.1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, wynik = 0, suma;
+
+    cout << "Wprowadz n: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        suma = 0;
+        for (int j = 1; j <= i; j++)
+        {
+            wynik += j;
+            suma += j;
+            cout << j;
+            if (j < i)
+            {
+                cout << "+";
+            }
+        }
+        cout << " = " << suma << endl;
+    }
+    cout << "Suma szeregu wynosi: " << wynik << endl;
+
+    return 0;
+}
---
+++ Zadanie2.3.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int month;
+
+    cout << "Podaj numer miesiaca z przedzialu od 1 do 12: ";
+    cin >> month;
+    cout << endl;
+
+    switch (month)
+    {
+    case 1:
+        cout << "Styczen";
+        break;
+    case 2:
+        cout << "Luty";
+        break;
+    case 3:
+        cout << "Marzec";
+        break;
+    case 4:
+        cout << "Kwiecien";
+        break;
+    case 5:
+        cout << "Maj";
+        break;
+    case 6:
+        cout << "Czewiec";
+        break;
+    case 7:
+        cout << "Lipiec";
+        break;
+    case 8:
+        cout << "Sierpien";
+        break;
+    case 9:
+        cout << "Wrzesien";
+        break;
+    case 10:
+        cout << "Pazdziernik";
+        break;
+    case 11:
+        cout << "Listopad";
+        break;
+    case 12:
+        cout << "Grudzien";
+        break;
+    default:
+        cout << "Wybrales liczbe spoza przedzialu.";
+    }
+    if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) {
+        cout << " ma 31 dni, ";
+    }
+    else if (month == 4 || month == 6 || month == 9 || month == 11) {
+        cout << " ma 30 dni, ";
+    }
+    else {
+        cout << " ma 28 dni, ";
+    }
+    
+    if (month >= 4 && month <= 9){
+        cout << "pogoda sloneczna." << endl;
+    }
+    
+    else {
+        cout << "pogoda pochmurna." << endl;
+    }
+        return 0;
+}
